4b263285f85fb4bd02de4037d233918b41b8514c
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-12-22 08:43:14 -0500

    Initial commit: Setup Spring Boot project structure for Fake Store API

---
+++ src/main/java/com/fakestore/api/FakeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.fakestore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fakestore/api/FakeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakestore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
