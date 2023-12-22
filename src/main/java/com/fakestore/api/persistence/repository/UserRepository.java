66fbb5ab06c138fdb13754a2da6bbec65bff0f3c
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-12-22 18:07:47 -0500

    feat(repositories): agregadas funciones menores en UserRepository

--- src/main/java/com/fakestore/api/persistence/repository/UserRepository.java
+++ src/main/java/com/fakestore/api/persistence/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.fakestore.api.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
 
