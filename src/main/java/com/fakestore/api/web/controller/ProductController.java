b89675d8802bb6f8b99566764e3124f90fd51717
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-12-27 18:46:34 -0500

    feat(ProductController): expande getAllProducts con filtrado y validaciones
    En ProductController, se ha expandido el endpoint `/all` para ofrecer:
    1. Soporte de paginación y filtrado avanzado, incluyendo parámetros como nombre, rango de precios, categoría, y más.
    2. Validaciones en los rangos de precios y cantidades, así como en las fechas, retornando `HttpStatus.BAD_REQUEST` en caso de datos inválidos.
    3. Conversión y validación de fechas utilizando `DateUtil`.

--- src/main/java/com/fakestore/api/web/controller/ProductController.java
+++ src/main/java/com/fakestore/api/web/controller/ProductController.java
@@ -5,11 +5,19 @@ import com.fakestore.api.dto.ProductCreationDTO;
 import com.fakestore.api.dto.ProductResponseDTO;
 import com.fakestore.api.persistence.entity.Product;
 import com.fakestore.api.service.ProductService;
+import com.fakestore.api.util.DateUtil;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -20,8 +28,53 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<ProductResponseDTO>> getAllProducts(){
-        List<ProductResponseDTO> productList = productService.getAllProducts();
+    public ResponseEntity<Page<ProductResponseDTO>> getAllProducts(
+            @PageableDefault(page =0, size =5) Pageable pageable,
+            @RequestParam(required = false) @Size(max = 255) String name,
+            @RequestParam(required = false) @Min(0) Double priceMin,
+            @RequestParam(required = false) @Min(0) Double priceMax,
+            @RequestParam(required = false) Long categoryId,
+            @RequestParam(required = false) @Min(0) Integer stockQuantityMin,
+            @RequestParam(required = false) @Min(0) Integer stockQuantityMax,
+            @RequestParam(required = false) String createdAtMin,
+            @RequestParam(required = false) String createdAtMax
+            ){
+
+        if (priceMin != null && priceMax != null) {
+            if (priceMin > priceMax) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        if (stockQuantityMin != null && stockQuantityMax != null) {
+            if (stockQuantityMin > stockQuantityMax) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        LocalDateTime minDate = null, maxDate = null;
+
+        if (createdAtMin != null && !createdAtMin.isEmpty()) {
+            try {
+                minDate = DateUtil.parseDateTime(createdAtMin);
+            } catch (DateTimeException e) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        if (createdAtMax != null && !createdAtMax.isEmpty()) {
+            try {
+                maxDate = DateUtil.parseDateTime(createdAtMax);
+            } catch (DateTimeException e) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        if (minDate != null && maxDate != null && !DateUtil.isValidDataRange(minDate, maxDate)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Page<ProductResponseDTO> productList = productService.getAllProducts(pageable, name, priceMin, priceMax, categoryId, stockQuantityMin, stockQuantityMax, minDate, maxDate);
         return new ResponseEntity<>(productList, HttpStatus.OK);
     }
 
