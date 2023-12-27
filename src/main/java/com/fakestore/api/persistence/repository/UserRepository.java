9d1b09f1bb7ec2d0e90f73ad98f41bf24d1d4ba3
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-12-27 18:41:37 -0500

    feat(UserRepository): añade método existsByEmail para verificar existencia de email

--- src/main/java/com/fakestore/api/persistence/repository/UserRepository.java
+++ src/main/java/com/fakestore/api/persistence/repository/UserRepository.java
@@ -10,5 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findOneByEmail(String email);
+
+    boolean existsByEmail(String mail);
 }
 
