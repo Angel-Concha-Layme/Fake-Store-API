972f98acd4f7ab8f8d1aa0ec5a53d3efadaca5a6
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-12-27 18:45:29 -0500

    feat(ProductRepository): añade métodos para verificación y búsqueda con especificaciones
    1. Método `existsByName` para verificar la existencia de un producto por su nombre. Esto facilita la validación y prevención de duplicados.
    2. Método `findAll` con parámetros `Specification<Product>` y `Pageable`, permitiendo búsquedas avanzadas y paginadas según especificaciones dinámicas.

--- src/main/java/com/fakestore/api/persistence/repository/ProductRepository.java
+++ src/main/java/com/fakestore/api/persistence/repository/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.fakestore.api.persistence.repository;
 
 import com.fakestore.api.persistence.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,8 @@ import java.util.Optional;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Object> findByName(String name);
+
+    boolean existsByName(String name);
+
+    Page<Product> findAll(Specification<Product> spec, Pageable pageable);
 }
