96fd7f2f60e07b15478bef25dd69ffba453ff06d
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-12-27 18:48:19 -0500

    feat(OrderRepository): actualiza findAllByUser para soportar paginación
    En OrderRepository, se realiza el siguiente cambio:
    - El método `findAllByUser` se modifica para aceptar un objeto `Pageable`. Ahora retorna `Page<Order>` en lugar de `List<Order>`, permitiendo la paginación de los resultados de las órdenes por usuario.

--- src/main/java/com/fakestore/api/persistence/repository/OrderRepository.java
+++ src/main/java/com/fakestore/api/persistence/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.fakestore.api.persistence.repository;
 
 import com.fakestore.api.persistence.entity.Order;
 import com.fakestore.api.persistence.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,5 @@ import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findAllByUser(User user);
+    Page<Order> findAllByUser(User user, Pageable pageable);
 }
