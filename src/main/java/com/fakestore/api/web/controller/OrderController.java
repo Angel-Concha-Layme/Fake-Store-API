ab2ada3b07101c9901c979224333ae4bd0f56bd0
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-12-27 18:52:30 -0500

    feat(OrderController): actualiza y expande funcionalidades de gestión de órdenes
    Se implementan varias actualizaciones y nuevas funcionalidades en `OrderController`:
    1. El endpoint `GET /api/orders/all` se modifica para incluir paginación.
    2. Se añade el endpoint `GET /api/orders/{id}` para obtener una orden específica, asegurando que solo el usuario que realizó la orden tenga acceso.
    3. Introducción de endpoints para cambiar el estado de las órdenes:
       - `PUT /api/orders/cancel/{id}` para cancelar una orden.
       - `PUT /api/orders/retake/{id}` para retomar una orden.
       - `PUT /api/orders/complete/{id}` para marcar una orden como completada.

--- src/main/java/com/fakestore/api/web/controller/OrderController.java
+++ src/main/java/com/fakestore/api/web/controller/OrderController.java
@@ -3,21 +3,23 @@ package com.fakestore.api.web.controller;
 import com.fakestore.api.dto.OrderDTO;
 import com.fakestore.api.dto.OrderDetailDTO;
 import com.fakestore.api.dto.OrderResponseDTO;
-import com.fakestore.api.persistence.entity.Order;
-import com.fakestore.api.persistence.entity.OrderDetail;
-import com.fakestore.api.persistence.entity.Product;
-import com.fakestore.api.persistence.entity.User;
+import com.fakestore.api.persistence.entity.*;
 import com.fakestore.api.service.OrderService;
 import com.fakestore.api.service.ProductService;
 import com.fakestore.api.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -27,31 +29,24 @@ public class OrderController {
     private final UserService userService;
     private final ProductService productService;
 
-    // ALL CRUD OPERATIONS:
-    // only users with JWT can access these endpoint methods
-
-    // GET /api/orders
     @GetMapping("/all")
     @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<List<OrderResponseDTO>> getAllOrders() {
+    public ResponseEntity<Page<OrderResponseDTO>> getAllOrders(
+            @PageableDefault(page = 0, size =5)Pageable pageable
+            ) {
         User user = userService.getAuthenticatedUser();
-        List<Order> orders = orderService.getAllOrdersByUser(user);
+        Page<Order> orders = orderService.getAllOrdersByUser(user, pageable);
 
-        List<OrderResponseDTO> response = new ArrayList<>();
-        for (Order order : orders) {
-            response.add(new OrderResponseDTO(
-                    order.getId(),
-                    order.getOrderDate().toString(),
-                    order.getOrderStatus().toString(),
-                    order.getTotal()
-            ));
-        }
+        Page<OrderResponseDTO> response = orders.map(order -> new OrderResponseDTO(
+                order.getId(),
+                order.getOrderDate().toString(),
+                order.getOrderStatus().toString(),
+                order.getTotal()
+        ));
+
         return ResponseEntity.ok(response);
     }
 
-
-    // GET /api/orders/{id}
-    // POST /api/orders
     @PostMapping("/create")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<OrderResponseDTO> createOrder(@RequestBody OrderDTO orderDTO) {
@@ -59,8 +54,8 @@ public class OrderController {
 
         Order order = new Order();
         order.setUser(user);
-        order.setOrderDate(orderDTO.orderDate());
-        order.setOrderStatus(orderDTO.orderStatus());
+        order.setOrderDate(LocalDate.now());
+        order.setOrderStatus(OrderStatus.PENDING);
         order.setOrderDetails(new ArrayList<>());
 
         for (OrderDetailDTO detailDTO : orderDTO.orderDetails()) {
@@ -89,6 +84,84 @@ public class OrderController {
         ));
     }
 
-    // PUT /api/orders/{id} // solo administradores
-    // DELETE /api/orders/{id} // solo administradores
+    @GetMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<OrderResponseDTO> getOrderById(@PathVariable Long id) {
+        User user = userService.getAuthenticatedUser();
+        Order order = orderService.getOrderById(id);
+
+        System.out.println("order.getUser().getId(): " + order.getUser().getId());
+        System.out.println("user.getId(): " + user.getId());
+
+        if (!Objects.equals(order.getUser().getId(), user.getId())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        return ResponseEntity.ok(new OrderResponseDTO(
+                order.getId(),
+                order.getOrderDate().toString(),
+                order.getOrderStatus().toString(),
+                order.getTotal()
+        ));
+    }
+
+    @PutMapping("/cancel/{id}")
+    @PreAuthorize(("isAuthenticated()"))
+    public ResponseEntity<OrderResponseDTO> cancelOrder(@PathVariable Long id) {
+        User user = userService.getAuthenticatedUser();
+        Order order = orderService.getOrderById(id);
+
+        if (!Objects.equals(order.getUser().getId(), user.getId())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        order.setOrderStatus(OrderStatus.CANCELLED);
+        Order savedOrder = orderService.saveOrder(order);
+        return ResponseEntity.ok(new OrderResponseDTO(
+                savedOrder.getId(),
+                savedOrder.getOrderDate().toString(),
+                savedOrder.getOrderStatus().toString(),
+                savedOrder.getTotal()
+        ));
+    }
+
+    @PutMapping("/retake/{id}")
+    @PreAuthorize(("isAuthenticated()"))
+    public ResponseEntity<OrderResponseDTO> retakeOrder(@PathVariable Long id) {
+        User user = userService.getAuthenticatedUser();
+        Order order = orderService.getOrderById(id);
+
+        if (!Objects.equals(order.getUser().getId(), user.getId())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        order.setOrderStatus(OrderStatus.PENDING);
+        Order savedOrder = orderService.saveOrder(order);
+        return ResponseEntity.ok(new OrderResponseDTO(
+                savedOrder.getId(),
+                savedOrder.getOrderDate().toString(),
+                savedOrder.getOrderStatus().toString(),
+                savedOrder.getTotal()
+        ));
+    }
+
+    @PutMapping("/complete/{id}")
+    @PreAuthorize(("isAuthenticated()"))
+    public ResponseEntity<OrderResponseDTO> completeOrder(@PathVariable Long id) {
+        User user = userService.getAuthenticatedUser();
+        Order order = orderService.getOrderById(id);
+
+        if (!Objects.equals(order.getUser().getId(), user.getId())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        order.setOrderStatus(OrderStatus.COMPLETED);
+        Order savedOrder = orderService.saveOrder(order);
+        return ResponseEntity.ok(new OrderResponseDTO(
+                savedOrder.getId(),
+                savedOrder.getOrderDate().toString(),
+                savedOrder.getOrderStatus().toString(),
+                savedOrder.getTotal()
+        ));
+    }
 }
